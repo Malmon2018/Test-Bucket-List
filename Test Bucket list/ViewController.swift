440005e2f569b2275bf06eddeb118bd28dbdfd95
Malavikha <malavikha93@gmail.com>
2018-04-06 07:24:47 +0800

    [Malavikha] Remove bucket view screen access

--- Test Bucket list/ViewController.swift
+++ Test Bucket list/ViewController.swift
@@ -64,8 +64,7 @@ class ViewController: UIViewController, UINavigationBarDelegate, UITableViewDele
     }
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         tableView.deselectRow(at: indexPath, animated: true)
-        myIndex = indexPath.row
-        performSegue(withIdentifier: "addOption", sender: self)
+        
         tableView.cellForRow(at: indexPath)?.accessoryType = UITableViewCellAccessoryType.disclosureIndicator
     }
     
